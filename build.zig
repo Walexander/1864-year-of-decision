ed6f16ca6345c3d7778e22eaff1738bfa9c5478c
Will Alexander <walexander@mco.com>
2024-07-23 08:55:41 -0400

    more, More, MORE memory please

--- build.zig
+++ build.zig
@@ -58,8 +58,10 @@ pub fn build(b: *std.Build) !void {
 
     lib.import_memory = true;
     lib.initial_memory = 65536;
-    lib.max_memory = 65536;
+    lib.max_memory = 131072;
     lib.stack_size = 14752;
+    // lib.max_memory = 65536;
+    // lib.stack_size = 14752;
 
     // Tests
     const allocator_tests = b.addTest(.{
